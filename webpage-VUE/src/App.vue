149cf53778fa4b318cff563a458c043811263e16
xavergruber <xavergruber@webundsoehne.com>
2022-08-03 11:50:55 +0200

    initial commit, template for webpage

---
+++ webpage-VUE/src/App.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+  <header>
+    This is where our navigation will take place.
+  </header>
+  <main>
+    This is the spot for our main content.
+  </main>
+  <footer>
+    And we will have a footer too.
+  </footer>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
